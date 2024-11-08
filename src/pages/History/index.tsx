74ae9e0a2f48dde217fde3706f39eb842f2ceacc
Renata Guerra <re.guerr@gmail.com>
2024-11-08 14:48:29 -0300

    Add status indicator for completed, pending, and interrupted states

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,19 +20,25 @@ export function History() {
               <td>Tarefa</td>
               <td>25 minutos</td>
               <td>Há 1 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa 2</td>
               <td>60 minutos</td>
               <td>Há 2 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>10 minutos</td>
               <td>Há 3 mês</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
