8cd061c16d3f9e34118cb578bfd2a5995652a6c7
Renata Guerra <re.guerr@gmail.com>
2024-11-20 15:29:08 -0300

    Add activity history functionality

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -15,6 +15,7 @@ interface Cycle {
 }
 
 interface CyclesContextType {
+  cycles: Cycle
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -68,8 +69,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    /* reset() */
   }
 
   function interruptCurrentCycle() {
@@ -88,6 +87,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         markCurrentCycleAsFinished,
